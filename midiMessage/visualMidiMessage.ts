21ded1826c68f8f0ab14f7a83c22fe05fb0c4138
ao <4040ao@gmail.com>
2025-04-02 11:19:37 +0900

    爆発のエフェクトをanimationに変更

--- midiMessage/visualMidiMessage.ts
+++ midiMessage/visualMidiMessage.ts
@@ -1,6 +1,7 @@
 import type {
 	ButtonSetting,
 	Color,
+	LightnessAnimation,
 	LightnessAnimationPage,
 	LightnessListList,
 	MIDIDevice,
@@ -47,6 +48,23 @@ const effectTypeBranch = async (
 		sendMidiMessageVertical(event, device, buttonSetting, coordinate);
 	} else if (buttonSetting.effectType === "explosion") {
 		sendMidiMessageExplosion(event, device, buttonSetting, coordinate);
+	}
+};
+
+const sendLightnessAnimation = async (
+	device: MIDIDevice,
+	colorType: Color["colorType"],
+	pressedCoordinate: { x: number; y: number },
+	lightnessAnimation: LightnessAnimation,
+) => {
+	for (const lightnessAnimationPage of lightnessAnimation.pages) {
+		sendLightnessAnimationPage(
+			device,
+			colorType,
+			pressedCoordinate,
+			lightnessAnimationPage,
+		);
+		await sleep(lightnessAnimation.duration);
 	}
 };
 
@@ -258,97 +276,69 @@ const sendMidiMessageExplosion = async (
 		return;
 	}
 
-	// ドットと1集周りが光る
-	for (let i = -1; i <= 1; i++) {
-		for (let j = -1; j <= 1; j++) {
-			sendLedMidiMessage(
-				device,
-				{
-					colorType: buttonSetting.colorType,
-					lightness: 1,
+	sendLightnessAnimation(device, buttonSetting.colorType, coordinate, {
+		pages: [
+			{
+				lightnessListList: [
+					[null, null, null, null, null, null, null],
+					[null, null, null, null, null, null, null],
+					[null, null, 1, 1, 1, null, null],
+					[null, null, 1, 1, 1, null, null],
+					[null, null, 1, 1, 1, null, null],
+					[null, null, null, null, null, null, null],
+					[null, null, null, null, null, null, null],
+				],
+				centerCoordinate: {
+					x: 3,
+					y: 3,
 				},
-				{ x: coordinate.x + i, y: coordinate.y + j },
-			);
-		}
-	}
-	await sleep(50);
-
-	// ドットと一周が消える
-	for (let i = -1; i <= 1; i++) {
-		for (let j = -1; j <= 1; j++) {
-			sendLedMidiMessage(
-				device,
-				{
-					colorType: "black",
-					lightness: 0,
+			},
+			{
+				lightnessListList: [
+					[null, null, null, null, null, null, null],
+					[null, 0.5, 0.5, 0.5, 0.5, 0.5, null],
+					[null, 0.5, 0, 0, 0, 0.5, null],
+					[null, 0.5, 0, 0, 0, 0.5, null],
+					[null, 0.5, 0, 0, 0, 0.5, null],
+					[null, 0.5, 0.5, 0.5, 0.5, 0.5, null],
+					[null, null, null, null, null, null, null],
+				],
+				centerCoordinate: {
+					x: 3,
+					y: 3,
 				},
-				{ x: coordinate.x + i, y: coordinate.y + j },
-			);
-		}
-	}
-
-	// ドットのさらに周りが光る
-	for (let i = -2; i <= 2; i++) {
-		for (let j = -2; j <= 2; j++) {
-			if (2 > Math.abs(i) && 2 > Math.abs(j)) {
-				continue;
-			}
-			sendLedMidiMessage(
-				device,
-				{
-					colorType: buttonSetting.colorType,
-					lightness: 0.1,
+			},
+			{
+				lightnessListList: [
+					[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+					[0.1, 0, 0, 0, 0, 0, 0.1],
+					[0.1, 0, null, null, null, 0, 0.1],
+					[0.1, 0, null, null, null, 0, 0.1],
+					[0.1, 0, null, null, null, 0, 0.1],
+					[0.1, 0, 0, 0, 0, 0, 0.1],
+					[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+				],
+				centerCoordinate: {
+					x: 3,
+					y: 3,
 				},
-				{ x: coordinate.x + i, y: coordinate.y + j },
-			);
-		}
-	}
-
-	await sleep(50);
-	// ドットのさらに周りが消える
-	for (let i = -2; i <= 2; i++) {
-		for (let j = -2; j <= 2; j++) {
-			sendLedMidiMessage(
-				device,
-				{
-					colorType: "black",
-					lightness: 0,
+			},
+			{
+				lightnessListList: [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, null, null, null, null, null, 0],
+					[0, null, null, null, null, null, 0],
+					[0, null, null, null, null, null, 0],
+					[0, null, null, null, null, null, 0],
+					[0, null, null, null, null, null, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+				],
+				centerCoordinate: {
+					x: 3,
+					y: 3,
 				},
-				{ x: coordinate.x + i, y: coordinate.y + j },
-			);
-		}
-	}
-
-	// ドットのさらにさらに周りが光る
-	for (let i = -3; i <= 3; i++) {
-		for (let j = -3; j <= 3; j++) {
-			if (3 > Math.abs(i) && 3 > Math.abs(j)) {
-				continue;
-			}
-			sendLedMidiMessage(
-				device,
-				{
-					colorType: buttonSetting.colorType,
-					lightness: 0.03,
-				},
-				{ x: coordinate.x + i, y: coordinate.y + j },
-			);
-		}
-	}
-
-	await sleep(50);
-	// ドットのさらに周りが消える
-	for (let i = -3; i <= 3; i++) {
-		for (let j = -3; j <= 3; j++) {
-			sendLedMidiMessage(
-				device,
-				{
-					colorType: "black",
-					lightness: 0,
-				},
-				{ x: coordinate.x + i, y: coordinate.y + j },
-			);
-		}
-	}
-	return;
+			},
+		],
+		duration: 50,
+	});
 };
